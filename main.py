6a543ab560b8cdabeea72badc626a87dd5802ea5
Litvish <79539377+Litvish@users.noreply.github.com>
2024-03-15 13:38:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 from Dot import Dot
-import numpy as np  # v 1.19.2
-import matplotlib.pyplot as plt  # v 3.3.2
+import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 import math
 import sklearn.metrics
@@ -33,14 +33,6 @@ def generate_test_dots(nuber):
         y.append(random.randint(1, 9) + random.random())
         color.append("#FFF673")
     return x, y, color
-
-
-# def generate_test_dots(number):
-#     x = [4,8,4,5]
-#     y = [1,6,9,5]
-#     # color = ['#007b25', '#025167', '#A61700', '#025167']
-#     color = ['#FFF673', '#FFF673', '#FFF673', '#FFF673']
-#     return x,y,color
 
 
 def create_dot_data(xs, ys, colors):
@@ -95,19 +87,15 @@ def color_dot(target_dot, knn):
     target_dot.color = dominate_color
     return target_dot
 
-
-# Enter x and y coordinates of points and colors
 xs, ys, colors = split_lines()
 dots = create_dot_data(xs, ys, colors)
 
 test_xs, test_ys, test_colors = generate_test_dots(4)
 test_dots = create_dot_data(test_xs, test_ys, test_colors)
 
-# Select length of axes and the space between tick labels
 xmin, xmax, ymin, ymax = 0, 10, 0, 10
 ticks_frequency = 1
 
-# Plot points
 fig, ax = plt.subplots(figsize=(10, 10))
 
 ax.scatter(xs, ys, c=colors)
@@ -122,36 +110,28 @@ for i in range(len(test_dots)):
 
 known_colors = ['#007B27', '#025176', '#A61709', '#025168']
 
-# Set identical scales for both axes
+
 ax.set(xlim=(xmin - 1, xmax + 1), ylim=(ymin - 1, ymax + 1), aspect='equal')
 
-# Set bottom and left spines as x and y axes of coordinate system
 ax.spines['bottom'].set_position('zero')
 ax.spines['left'].set_position('zero')
 
-# Remove top and right spines
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 
-# Create 'x' and 'y' labels placed at the end of the axes
 ax.set_xlabel('x', size=14, labelpad=-24, x=1.03)
 ax.set_ylabel('y', size=14, labelpad=-21, y=1.02, rotation=0)
 
-# Create custom major ticks to determine position of tick labels
 x_ticks = np.arange(xmin, xmax + 1, ticks_frequency)
 y_ticks = np.arange(ymin, ymax + 1, ticks_frequency)
 ax.set_xticks(x_ticks[x_ticks != 0])
 ax.set_yticks(y_ticks[y_ticks != 0])
 
-# Create minor ticks placed at each integer to enable drawing of minor grid
-# lines: note that this has no effect in this example with ticks_frequency=1
 ax.set_xticks(np.arange(xmin, xmax + 1), minor=True)
 ax.set_yticks(np.arange(ymin, ymax + 1), minor=True)
 
-# Draw major and minor grid lines
 ax.grid(which='both', color='grey', linewidth=1, linestyle='-', alpha=0.2)
 
-# Draw arrows
 arrow_fmt = dict(markersize=4, color='black', clip_on=False)
 ax.plot(1, 0, marker='>', transform=ax.get_yaxis_transform(), **arrow_fmt)
 ax.plot(0, 1, marker='^', transform=ax.get_xaxis_transform(), **arrow_fmt)
